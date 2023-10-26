24f7cf41475a49e19ca734c3afad476638eb9ec4
alejandrocrespopuche <alejandrocrespo.alu@iespacomolla.es>
2023-10-26 14:12:52 +0200

    primera version

--- app/src/main/java/com/example/segundoproyecto/ActivityResultado.kt
+++ app/src/main/java/com/example/segundoproyecto/ActivityResultado.kt
@@ -1,12 +1,14 @@
 package com.example.segundoproyecto
 
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 
 
@@ -14,24 +16,41 @@ class ActivityResultado : ComponentActivity() {
 
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_resultado)
         val tvResult = findViewById<TextView>(R.id.tvResult)
+        val tvResult2 = findViewById<TextView>(R.id.tvResult2)
+
         val name: String = intent.extras?.getString("EXTRA_NAME").orEmpty()
+        val f: String = intent.extras?.getString("HORA").orEmpty()
         val volver= findViewById<Button>(R.id.volver)
 
         tvResult.text = getString(R.string.hola, name)
+        tvResult2.text = f
+        val intent = Intent(this, MainActivity::class.java)
 
-        volver.setOnClickListener {
+        volver.setOnClickListener()
+        {
 
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            val alerta= AlertDialog.Builder(this)
+            val alerta = AlertDialog.Builder(this)
             alerta.setTitle("ALERTA")
-            alerta.setMessage("Estas seguro de volver????")
-            alerta.setPositiveButton(android.R.string.ok){ dialogInterface: DialogInterface, i: Int -> }
+            alerta.setMessage("¿Estás seguro de volver?")
+            alerta.setPositiveButton(android.R.string.ok) { dialog, which ->
+
+                Toast.makeText(applicationContext, android.R.string.ok, Toast.LENGTH_SHORT).show()
+                startActivity(intent)
+            }
+            alerta.setNegativeButton(android.R.string.cancel) { dialog, which ->
+                Toast.makeText(applicationContext, android.R.string.cancel, Toast.LENGTH_SHORT).show() }
+
+            alerta.setNeutralButton("omitir") { dialog, which ->
+                Toast.makeText(applicationContext, "has pulsado omitir", Toast.LENGTH_SHORT).show() }
+
             alerta.show()
+
+
         }
     }
 }
--- app/src/main/java/com/example/segundoproyecto/MainActivity.kt
+++ app/src/main/java/com/example/segundoproyecto/MainActivity.kt
@@ -4,6 +4,7 @@ package com.example.segundoproyecto
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.DatePicker
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -12,6 +13,7 @@ import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
 
+    lateinit var fec : DatePicker
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -19,16 +21,22 @@ class MainActivity : ComponentActivity() {
         val boton = findViewById<Button>(R.id.boton1)
         val texto = findViewById<EditText>(R.id.texto1)
 
+
         boton.setOnClickListener {
             val nombre = texto.text.toString()
 
             if (nombre.isNotEmpty()) {
                 val intent = Intent(this, ActivityResultado::class.java)
                 intent.putExtra("EXTRA_NAME", nombre)
+                intent.putExtra("HORA", fecha())
                 startActivity(intent)
                 Toast.makeText(this, "actividad 2", Toast.LENGTH_SHORT).show()
             }
         }
 
+    }
+    fun fecha() : String {
+        fec= findViewById(R.id.cal)
+        return "Fecha: "+fec.dayOfMonth.toString()+"/"+fec.month.toString()+"/"+fec.year.toString()
     }
 }
